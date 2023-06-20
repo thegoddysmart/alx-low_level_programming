bc4acfc5e4ee44d81cead38acb7d03fbe3d84d79
Godfred <goddysmart2468@gmail.com>
2023-06-20 16:30:11 -0700

    debug

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; j++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
@@ -17,19 +17,17 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
-		}	if (k < 10 && j != 0)
+			else
 			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(k + '0');
-			} else if (k >= 10)
-			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				if (k < 10)
+					_putchar(' ');
+				else
+					_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
