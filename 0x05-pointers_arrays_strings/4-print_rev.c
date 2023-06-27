79227a29cdfc98febf5250fbff6162e7984b4421
Godfred <goddysmart2468@gmail.com>
2023-06-26 19:22:03 -0700

    prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string parameter input
+ * Return: Nothing
+ */
+
+void print_rev(char *)
+{
+	int index = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		index++;
+		s++;
+	}
+
+	s--;
+	for (o = index; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
