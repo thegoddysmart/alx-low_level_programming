5702f85b8bf7e8ae8b86ea06c3a7f6c3938658b6
Godfred <goddysmart2468@gmail.com>
2023-06-27 02:16:03 -0700

    debug

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,11 +12,17 @@ void puts_half(char *str)
 
 	index = 0;
 	for (l = 0; str[l] != '\0'; l++)
+	{
 		index++;
+	}
 	n = (index / 2);
 	if ((index % 2) == 1)
+	{
 		n = ((index + 1) / 2);
+	}
 	for (l = n; str[l] != '\0'; l++)
+	{
 		_putchar(str[l]);
+	}
 	_putchar('\n');
 }
