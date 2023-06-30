d5f3c44a4a8ed01c9707493521bfa9c1ed90529e
Godfred <goddysmart2468@gmail.com>
2023-06-29 21:28:46 -0700

    debug

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -19,7 +19,7 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 	j = 0;
-	while (src[i] != '\0')
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
@@ -27,5 +27,5 @@ char *_strcat(char *dest, char *src)
 	}
 
 	dest[i] = '\0';
-	return (dest)
+	return (dest);
 }
