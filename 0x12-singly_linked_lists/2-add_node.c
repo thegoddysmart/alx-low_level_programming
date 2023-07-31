b43f7ddb1371ed93702a13b86059b3997390e3da
Godfred <goddysmart2468@gmail.com>
2023-07-31 10:55:27 -0700

    debug

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,10 +12,11 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	if (str === NULL)
+	if (str == NULL)
 		return (NULL);
 
 	list_t *new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
