5d3a92ef501e7c9cccfdeacebea79252013023ee
Godfred <goddysmart2468@gmail.com>
2023-09-04 22:23:50 -0700

    debug

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	op = open(filename, O_WRONLY | O_APPEND);
-	wf = write(o, text_content, len);
+	wf = write(op, text_content, len);
 
 	if (op == -1 || wf == -1)
 		return (-1);
