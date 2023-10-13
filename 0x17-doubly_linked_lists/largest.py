4648e7d2e33643711dd8a101b33cd2c4925f6e70
Godfred <goddysmart2468@gmail.com>
2023-10-12 18:33:49 -0700

    debug

---
+++ 0x17-doubly_linked_lists/largest.py
@@ -0,0 +1,11 @@
+largest_palindrome = 0
+
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        product = i * j
+        if (str(product) == str(product)[::-1] and product > largest_palindrome):
+            largest_palindrome = product
+
+# Write the result to a file
+with open("102-result", "w") as result_file:
+    result_file.write(str(largest_palindrome))
